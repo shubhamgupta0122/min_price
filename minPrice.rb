3bf1c6e20c47928d99edf55976bebdbac822c883
Shubham Gupta <shubhamgupta0122@hotmail.com>
2016-02-29 14:35:35 +0530

    Added basic error handling. Separated driver and business logic.

--- minPrice.rb
+++ minPrice.rb
@@ -2,35 +2,35 @@ require 'mechanize'
 require 'yaml'
 
 class MinPrice
+	
 	def initialize()
-		
 		@agent = Mechanize.new
 		@sites = YAML.load_file('sites.yml')
-
 	end
 
 	def search_site(qword, qsite)
 		qsite = @sites.select { |site| site[:name] == qsite }
 		qsite = qsite[0]
+
 		page = @agent .get ( qsite[:url] .sub("%s", qword) )
-		name = page .at_xpath( qsite[:item_name] ) .text .strip
-		price = page .at_xpath( qsite[:item_price] ) .text
-		price = price .sub( "Rs." , "" ) .delete(",") .strip .delete(" ") .to_i
-		{name: name, price: price}
+		
+		name = page .at_xpath( qsite[:item_name] )
+		price = page .at_xpath( qsite[:item_price] )
+		
+		if ( name && price )
+			name = name.text.strip
+			price = price.text.sub("Rs.","").delete(",").delete(" ").strip.to_i
+			{name: name, price: price}
+		else
+			"Error Occured"
+		end
 	end
 
 	def search_all(qword)
 		@sites.each do |site|
 			puts "Searching #{site[:name]}..."
-			puts "#{site[:name]} => \n#{search_site(qword, site[:name])}\n\n-----------"
+			puts "#{search_site(qword, site[:name])}\n\n-----------"
 		end
 	end
 
-end
-
-h = MinPrice.new
-
-ARGV.each do |q|
-	puts "\n\nSearching for #{q}\n"
-	h.search_all("#{q}")
 end
---
+++ minPriceDriver.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+require './minPrice.rb'
+
+min = MinPrice.new
+
+ARGV.each do |q|
+
+	puts "\nSearching for #{q}\n"
+	print "--------------"
+	q.length.times { print "-" }
+	print "\n"
+
+	# Call the actual logic
+	min.search_all("#{q}")
+end
